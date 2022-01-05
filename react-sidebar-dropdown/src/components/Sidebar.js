4c0a2e69e3211d8676f4ec140957b97b9e554a1b
CamilaNunes <cvn.camila@gmail.com>
2022-01-05 15:07:16 -0300

    Começando o layout;

--- react-sidebar-dropdown/src/components/Sidebar.js
+++ react-sidebar-dropdown/src/components/Sidebar.js
@@ -4,11 +4,27 @@ import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 
+const Nav = styled.div `
+  background: #15171C;
+  heigth: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+`;
+
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
-    </div>
+    <>
+      <Nav>
+        <NavIcon to="#">
+          <FaIcons.FaBars/>
+        </NavIcon>
+      </Nav>
+    </>
   )
 }
 
