ae3cc89ed755c956abceb464867805496e5317c6
CamilaNunes <cvn.camila@gmail.com>
2022-01-06 18:42:50 -0300

    Criando barra de navegação;

--- react-sidebar-dropdown/src/components/Sidebar.js
+++ react-sidebar-dropdown/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
@@ -6,7 +6,7 @@ import * as AiIcons from 'react-icons/ai';
 
 const Nav = styled.div `
   background: #15171C;
-  heigth: 80px;
+  height: 80px;
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -14,16 +14,48 @@ const Nav = styled.div `
 
 const NavIcon = styled(Link)`
   margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171C; 
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+  trasition: 750ms;
+  z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+  width: 100%;
 `;
 
 const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar)
+
   return (
     <>
       <Nav>
         <NavIcon to="#">
-          <FaIcons.FaBars/>
+          <FaIcons.FaBars onClick={showSidebar}/>
         </NavIcon>
       </Nav>
+      <SidebarNav sidebar={sidebar}>
+        <SidebarWrap>
+        <NavIcon to="#">
+          <AiIcons.AiOutlineClose/>
+        </NavIcon>    
+        </SidebarWrap> 
+      </SidebarNav>
     </>
   )
 }
